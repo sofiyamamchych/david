fba0874fef5534a3260614ab1b954282dc0dd5fa
Sofiya Mamchych <sofiyamamchich@gmail.com>
2018-02-21 23:24:04 +0200

    carousel pagenation draft

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import 'slick-carousel';
 
 $(document).ready(function() {
   $('.carousel').slick({
+    dots: true,
     prevArrow: '.carousel__prevpng',
     nextArrow: '.carousel__nextpng'
   });
